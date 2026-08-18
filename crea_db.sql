0d15be3f8b7d84ea81bd7d72602cc40d50821335
Marco Santoro <santoromarco@gmail.com>
2026-08-18 22:02:16 +0200

    Crea db

---
+++ crea_db.sql
@@ -0,0 +1,16 @@
+ -- Creazione e selezione del Database
+        CREATE DATABASE IF NOT EXISTS algora_db CHARACTER SET utf8mb4
+        COLLATE utf8mb4_unicode_ci;
+        USE algora_db;
+
+        -- Creazione della tabella per i contatti dal form web
+        CREATE TABLE IF NOT EXISTS contatti (
+
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                nome VARCHAR(100) NOT NULL,
+                ente VARCHAR(100),
+                email VARCHAR(100) NOT NULL,
+                tipo VARCHAR(50),
+                messaggio TEXT NOT NULL,
+                data_invio DATETIME DEFAULT CURRENT_TIMESTAMP
+        ) ENGINE=InnoDB;
